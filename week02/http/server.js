feffd59e1b8151bad896d46a1f288cad3dd7ce6f
陈禹 <1105811473@qq.com>
2020-08-10 00:01:57 +0800

    add week02 homework

--- week02/http/server.js
+++ week02/http/server.js
@@ -12,7 +12,7 @@ http.createServer((request,response)=>{
         response.writeHead(200,{
             'Content-Type':'text/html'
         });
-        response.end('hello world' + body);
+        response.end('hello world');
     })
 }).listen(3000);
 
